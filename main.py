f0336f528dc9f7a4003a34a8e6eab2958901dfe5
Leronsy <iminnominate@gmail.com>
2018-06-03 13:36:23 +0300

    Запуск через функцию main

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import itertools as itt
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_wine, load_boston
 from sklearn.linear_model import LinearRegression
-# from sklearn.datasets import load_wine
-# from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
@@ -18,11 +17,9 @@ def classification_errors_counter(predicted, known):
     return errors
 
 
-def compare_regression_mse(current, correct, best):
+def compare_regression_mse(current, correct):
     mse = mean_squared_error(current, correct)
-    if mse < best:
-        best = mse
-    return mse, best
+    return mse
 
 
 def printlist(list_for_print):
@@ -32,72 +29,72 @@ def printlist(list_for_print):
     print("-" * 20)
 
 
-# features, target = load_wine(return_X_y=True)
-# clf = GaussianNB()
+def main(dataset, classifier, error_function):
+    features, target = dataset(return_X_y=True)
+    clf = classifier
+    err_fun = error_function
 
-features, target = load_boston(return_X_y=True)
-clf = LinearRegression()
+    x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.30)
 
-X_train, X_test, y_train, y_test = train_test_split(features, target,
-                                                    test_size=0.30)
-dimension = features.shape[1]
-combinations_amount = combinations_sum(dimension) - 1
-combination_length_lst = [n for n in range(dimension)]
+    dimension = features.shape[1]
+    combinations_amount = combinations_sum(dimension) - 1
+    combination_length_lst = [n for n in range(dimension)]
 
-errors_train = np.zeros(combinations_amount)
-errors_test = np.zeros(combinations_amount)
+    errors_train = np.zeros(combinations_amount)
+    errors_test = np.zeros(combinations_amount)
 
-print('Перебор из {0} наборов признаков.'.format(combinations_amount))
-min_by_len = np.zeros((2,dimension))
-features_lst = list()
-best_train = float('Inf')
-best_test = float('Inf')
-i = 0
-sum=0
-for length in range(1, dimension + 1):
-    i_prev = i
-    for subset in itt.combinations(combination_length_lst, length):
-        columns = list(subset)
-        features_lst.append(columns)
+    print('Перебор из {0} наборов признаков.'.format(combinations_amount))
 
-        X_train_part = X_train[:, columns]
-        X_test_part = X_test[:, columns]
+    plt.figure(figsize=(6, 7), dpi=200)
+    min_by_len = np.zeros((2, dimension))
+    features_lst = list()
+    i = 0
+    for length in range(1, dimension + 1):
+        i_prev = i
+        for subset in itt.combinations(combination_length_lst, length):
+            columns = list(subset)
+            features_lst.append(columns)
 
-        clf.fit(X_train_part, y_train)
+            x_train_part = x_train[:, columns]
+            x_test_part = x_test[:, columns]
 
-        predict_train = clf.predict(X_train_part)
-        predict_test = clf.predict(X_test_part)
+            clf.fit(x_train_part, y_train)
 
-        # errors_train[i] = classification_errors_counter(predict_train, y_train)
-        # errors_test[i] = classification_errors_counter(predict_test, y_test)
-        errors_train[i], best_train = compare_regression_mse(predict_train, y_train, best_train)
-        errors_test[i], best_test = compare_regression_mse(predict_test, y_test, best_test)
-        # print(errors_test[i],'|',best_test)
-        i += 1
-    plt.scatter([length for _ in range(i_prev,i)], errors_test[i_prev:i])
-    sum +=i-i_prev
-    min_by_len[0,length-1]=length
-    min_by_len[1,length-1]=errors_test[i_prev:i].min()
+            predict_train = clf.predict(x_train_part)
+            predict_test = clf.predict(x_test_part)
 
-plt.axhline(errors_test.min(),ls='dashed', c='red')
-plt.plot(min_by_len[0],min_by_len[1],c='grey')
-plt.axis([0,dimension+1,errors_test.min()-1,errors_test.max()+1])
+            errors_train[i] = err_fun(predict_train, y_train)
+            errors_test[i] = err_fun(predict_test, y_test)
 
-minimums_train = np.where(errors_train == errors_train.min())
-minimums_test = np.where(errors_test == errors_test.min())
-best_col_set_test = features_lst[minimums_test[0][0]]
-columns_minimal_number = len(best_col_set_test)
-col_lst = []
-for i in minimums_test[0]:
-    lst = features_lst[i]
+            i += 1
+        plt.scatter([length for _ in range(i_prev, i)], errors_test[i_prev:i], s=20)
+        min_by_len[0, length - 1] = length
+        min_by_len[1, length - 1] = errors_test[i_prev:i].min()
 
-    if len(lst) == columns_minimal_number:
-        col_lst.append(lst)
+    plt.axhline(errors_test.min(), ls='dashed', c='red')
+    plt.plot(min_by_len[0], min_by_len[1], c='grey')
+    plt.axis([0, dimension + 1, errors_test.min() - 1, errors_test.max() + 1])
 
-plt.legend(('min = '+str("{0:3.2f}").format(errors_test.min()),best_col_set_test))
-plt.show()
-print("Лучшие минимальные наборы по тестам:\t")
-printlist(col_lst)
-print("Ошибки у наборов\t train | test")
-print('{0:17s}\t{1:6.2f} |{2:5.2f}'.format("Лучшие", errors_train.min(), errors_test.min()))
-print('{0:17s}\t{1:6.2f} |{2:5.2f}'.format("Полный", errors_train[-1], errors_test[-1]))
+    minimums_test = np.where(errors_test == errors_test.min())
+    best_col_set_test = features_lst[minimums_test[0][0]]
+    columns_minimal_number = len(best_col_set_test)
+    col_lst = []
+    for i in minimums_test[0]:
+        lst = features_lst[i]
+        if len(lst) == columns_minimal_number:
+            col_lst.append(lst)
+
+    plt.legend(
+        ('min = ' + str("{0:3.2f} on {1:2d} p").format(errors_test.min(), len(best_col_set_test)),))
+    plt.show()
+
+    print("Лучшие минимальные наборы по тестам:\t")
+    printlist(col_lst)
+    print("Ошибки у наборов\t train | test")
+    print('{0:17s}\t{1:6.2f} |{2:5.2f}'.format("Лучшие", errors_train.min(), errors_test.min()))
+    print('{0:17s}\t{1:6.2f} |{2:5.2f}'.format("Полный", errors_train[-1], errors_test[-1]))
+
+
+if __name__ == '__main__':
+    main(load_wine, GaussianNB(), classification_errors_counter)
+    # main(load_boston, LinearRegression(), compare_regression_mse)
